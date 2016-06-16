21db818f62c934571372e4d71841cdb3a7c06ff1
j129008 <j129008@gmail.com>
2016-06-17 06:02:40 +0800

    add danny py

---
+++ big_data_danny.py
@@ -0,0 +1,131 @@
+from os import listdir
+from os.path import isfile, join
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import BernoulliNB
+import pickle
+
+# My modules
+from sklearn.externals.six.moves import zip
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+from sklearn.linear_model import SGDClassifier
+
+# ----------------------- prepare data -------------- #
+# # FIXME change your data path/folder here
+
+train_file = './train' # folder
+test_file = './test' # folder
+
+pred_fname = './submission_SGDClassifier.csv' # predicitons
+
+# ##########################################
+
+tr_ans = []
+tr_vec = []
+te_vec = []
+te_ids = []
+
+print('load training data ...')
+
+for line in open(train_file,'r'):
+	fields = line.split(',')
+	tr_ans.append(fields[1])
+	data = [int(fields[2][4:6])]
+	for x in range(3,len(fields)):
+		data.append(fields[x])
+	tr_vec.append(data)
+
+print('load testing data ...')
+
+for line in open(test_file):
+	fields = line.split(',')
+	te_ids.append(fields[0])
+	data = [int(fields[1][4:6])]
+	for y in range(2,len(fields)):
+		data.append(fields[y])
+	te_vec.append(data)
+
+print('transform data to index ...')
+
+indexDict = dict()
+for i in range(0,len(tr_vec)):
+	for j in range(1,len(tr_vec[i])):
+		if not(str(j) in indexDict):
+			indexDict[str(j)] = dict()
+			indexDict[str(j)]['all_count'] = 0
+		if not(tr_vec[i][j] in indexDict[str(j)]):
+			indexDict[str(j)][tr_vec[i][j]] = indexDict[str(j)]['all_count']
+			indexDict[str(j)]['all_count'] = indexDict[str(j)]['all_count'] + 1
+
+for i in range(0,len(te_vec)):
+	for j in range(1,len(te_vec[i])):
+		if not(te_vec[i][j] in indexDict[str(j)]):
+			indexDict[str(j)][te_vec[i][j]] = indexDict[str(j)]['all_count']
+			indexDict[str(j)]['all_count'] = indexDict[str(j)]['all_count'] + 1
+
+print('put index in data ...')
+
+for x in range(0,len(tr_vec)):
+	for y in range(1,len(tr_vec[x])):
+		tr_vec[x][y] = indexDict[str(y)][tr_vec[x][y]]
+
+for x in range(0,len(te_vec)):
+	for y in range(1,len(te_vec[x])):
+		te_vec[x][y] = indexDict[str(y)][te_vec[x][y]]
+
+print(tr_vec)
+train_file = open('train_vec.pkl','wb');
+pickle.dump(tr_vec,train_file)
+train_file.close();
+trn_ans = open('train_ans.pkl','wb');
+pickle.dump(tr_ans,trn_ans)
+train_file.close();
+test_file = open('test_vec.pkl','wb');
+pickle.dump(te_vec,test_file)
+train_file.close();
+te_ids_file = open('te_ids.pkl','wb')
+pickle.dump(te_ids,te_ids_file)
+te_ids_file.close()
+
+print('ok')
+# ----------------------- just load this time -------------- #
+
+# print('load training data ...')
+# train_file = open('train_vec.pkl','r');
+# tr_vec = pickle.load(train_file)
+# train_file.close();
+# print('load labels data ...')
+# trn_ans = open('train_ans.pkl','r');
+# tr_ans = pickle.load(trn_ans)
+# trn_ans.close();
+# print('load testing data ...')
+# test_file = open('test_vec.pkl','r');
+# te_vec = pickle.load(test_file)
+# test_file.close();
+# print('load testing data ...')
+# te_ids_file = open('te_ids.pkl','r');
+# te_ids = pickle.load(te_ids_file)
+# te_ids_file.close();
+
+print('build SGD classifier ...')
+clf = SGDClassifier(loss="log", penalty="l2", n_jobs="4")
+clf.fit(tr_vec, tr_ans)
+
+sgd_file = open('sgd_model.pkl','wb')
+pickle.dump(clf,sgd_file)
+sgd_file.close()
+print('save model done')
+
+print('make predictions ...')
+clf_predictions = clf.predict(te_vec)
+
+print('store predictions in <%s>', pred_fname)
+# print(clf_predictions)
+result = ''
+for x in range(0,len(clf_predictions)):
+    result = result  +  te_ids[x] + ',' + str(clf_predictions[x]) + '\n'
+with open(pred_fname, 'w') as f:
+    f.write(result)
