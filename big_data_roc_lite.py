3034956b9773e79fbab038c9b7e068b0e0e70e8d
j129008 <j129008@gmail.com>
2016-06-19 17:51:12 +0800

    add counter feature

--- big_data_roc_lite.py
+++ big_data_roc_lite.py
@@ -37,77 +37,70 @@ te_ids = []
 print('load training data ...')
 
 index = dict()
-for i in range(3, 24):
+counter = dict()
+for i in range(2, 24):
     index[i] = dict()
+    counter[i] = dict()
 
+# counter
 for line in open(train_file,'r'):
     fields = line.split(',')
     fields[-1] = int(fields[-1].strip())
     tr_ans.append(fields[1])
-    data = [int(fields[2][6:8])]
-    for featureID in range(3,len(fields)):
+    fields[2] = int(fields[2][6:8])
+    for featureID in range(2,len(fields)):
+        featureType = fields[featureID]
+        try:
+            counter[featureID][featureType] += 1
+        except:
+            counter[featureID][featureType] = 1
+
+# train
+for line in open(train_file,'r'):
+    fields = line.split(',')
+    fields[-1] = int(fields[-1].strip())
+    fields[2] = int(fields[2][6:8])
+    countAll = 0
+    data = []
+    for featureID in range(2,len(fields)):
         try:
             data.append(index[featureID][fields[featureID]])
         except:
             index[featureID][fields[featureID]] = len(index[featureID])
             data.append(index[featureID][fields[featureID]])
-    tr_vec.append(data)
+        if featureID == 2 or featureID == 11 or featureID ==12:
+            countAll += counter[featureID][fields[featureID]]
+    tr_vec.append(data + [countAll])
 
 print('load testing data ...')
 
+# test
+"""
 for line in open(test_file):
     fields = line.split(',')
     fields[-1] = int(fields[-1].strip())
     te_ids.append(fields[0])
-    data = [int(fields[1][6:8])]
-    for featureID in range(2,len(fields)):
+    fields[1] = int(fields[1][6:8])
+    countAll = 0
+    data = []
+    for featureID in range(1,len(fields)):
         try:
             data.append(index[featureID+1][fields[featureID]])
         except:
             index[featureID+1][fields[featureID]] = len(index[featureID+1])
             data.append(index[featureID+1][fields[featureID]])
-    te_vec.append(data)
+        if featureID == 1 or featureID == 10 or featureID ==11:
+            try:
+                countAll += counter[featureID+1][fields[featureID]]
+            except:
+                pass
+    te_vec.append(data+[countAll])
+"""
 
-# pprint(tr_vec)
-# pprint(index)
-# pprint(te_vec)
-# input()
-# train_file = open('train_vec.pkl','wb');
-# pickle.dump(tr_vec,train_file)
-# train_file.close();
-# trn_ans = open('train_ans.pkl','wb');
-# pickle.dump(tr_ans,trn_ans)
-# train_file.close();
-# test_file = open('test_vec.pkl','wb');
-# pickle.dump(te_vec,test_file)
-# train_file.close();
-# te_ids_file = open('te_ids.pkl','wb')
-# pickle.dump(te_ids,te_ids_file)
-# te_ids_file.close()
-
-print('ok')
-# ----------------------- just load this time -------------- #
-
-# print('load training data ...')
-# train_file = open('train_vec.pkl','r');
-# tr_vec = pickle.load(train_file)
-# train_file.close();
-# print('load labels data ...')
-# trn_ans = open('train_ans.pkl','r');
-# tr_ans = pickle.load(trn_ans)
-# trn_ans.close();
-# print('load testing data ...')
-# test_file = open('test_vec.pkl','r');
-# te_vec = pickle.load(test_file)
-# test_file.close();
-# print('load testing data ...')
-# te_ids_file = open('te_ids.pkl','r');
-# te_ids = pickle.load(te_ids_file)
-# te_ids_file.close();
-
-print('build SGD classifier ...')
+print('build classifier ...')
 # clf = SGDClassifier(loss="log", penalty="l2", n_jobs="4")
 clf = RandomForestClassifier(n_jobs=4)
+# clf.fit(tr_vec[1000000:], tr_ans[1000000:])
 clf.fit(tr_vec[1000000:], tr_ans[1000000:])
 
 # print(cross_validation.cross_val_score(clf, tr_vec, tr_ans, scoring='log_loss'))
@@ -130,9 +123,8 @@ print('make predictions ...')
 # f.write('id,click\n')
 # for x in range(0,len(clf_predictions)):
     # f.write(te_ids[x] + ',' + str(clf_predictions[x][1])+'\n')
-
-clf_predictions = clf.predict_proba(tr_vec[:1000000])
-fpr, tpr, thresholds = roc_curve(tr_ans[:1000000], [x[1] for x in clf_predictions], pos_label='1')
+clf_predictions = clf.predict_proba(tr_vec[:1000001])
+fpr, tpr, thresholds = roc_curve(tr_ans[:1000001], [x[1] for x in clf_predictions], pos_label='1')
 fpr_new = []
 tpr_new = []
 # get_through = 10
