0e489457393e83f96003a1c220ee9aa98cf3d6e2
j129008 <j129008@gmail.com>
2016-06-22 23:25:25 +0800

    print error rate / tree

--- big_data_roc_lite_spark.py
+++ big_data_roc_lite_spark.py
@@ -11,12 +11,18 @@ data = MLUtils.loadLibSVMFile( sc, '/home/bigdata/hadoop-2.7.2/data/trainOut' )
 (trainingData, testData) = data.randomSplit([0.7, 0.3])
 
 print('build classifier ...')
-clf = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
+model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
         numTrees=3, featureSubsetStrategy="auto",
         impurity='gini', maxDepth=4, maxBins=32)
 
-# Evaluate model on test instances and compute test error
-predictionAndLabels = testData.map(lambda lp: (float(clf.predict(lp.features)), lp.label))
+print('start predict ...')
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
+print('Test Error = ' + str(testErr))
+print('Learned classification forest model:')
+print(model.toDebugString())
+
 """
 # Instantiate metrics object
 metrics = BinaryClassificationMetrics(predictionAndLabels)
