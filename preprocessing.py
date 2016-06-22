dfb66ccb0f02d1c520c3532ed941fab4a0ab348c
j129008 <j129008@gmail.com>
2016-06-22 16:23:42 +0800

    merge ans to training data

--- preprocessing.py
+++ preprocessing.py
@@ -3,15 +3,7 @@ test_file = './testLite'
 
 train_out = './data/trainOut'
 test_out = './data/testOut'
-ans_out = './data/ansOut'
 
-
-tr_ans = []
-tr_vec = []
-te_vec = []
-te_ids = []
-
-print('load training data ...')
 
 index = dict()
 counter = dict()
@@ -19,12 +11,11 @@ for i in range(2, 24):
     index[i] = dict()
     counter[i] = dict()
 
+print('build feature counter...')
 # counter
-fOut = open(ans_out, 'w')
 for line in open(train_file,'r'):
     fields = line.split(',')
     fields[-1] = int(fields[-1].strip())
-    # tr_ans.append(fields[1])
     fields[2] = int(fields[2][6:8])
     for featureID in range(2,len(fields)):
         featureType = fields[featureID]
@@ -32,10 +23,9 @@ for line in open(train_file,'r'):
             counter[featureID][featureType] += 1
         except:
             counter[featureID][featureType] = 1
-    fOut.write(fields[1] + '\n')
-fOut.close()
 
 # train
+print('indexing training data...')
 fOut = open(train_out, 'w')
 for line in open(train_file,'r'):
     fields = line.split(',')
@@ -51,17 +41,15 @@ for line in open(train_file,'r'):
             data += str(index[featureID][fields[featureID]])
         if featureID == 2 or featureID == 11 or featureID ==12:
             countAll += counter[featureID][fields[featureID]]
-    fOut.write( data + ',' + str(countAll) + '\n' )
+    fOut.write( str(fields[1]) + ',' + data + ',' + str(countAll) + '\n' )
 fOut.close()
 
-print('load testing data ...')
-
+print('indexing testing data...')
 # test
 fOut = open(test_out, 'w')
 for line in open(test_file):
     fields = line.split(',')
     fields[-1] = int(fields[-1].strip())
-    te_ids.append(fields[0])
     fields[1] = int(fields[1][6:8])
     countAll = 0
     data = ''
@@ -76,6 +64,5 @@ for line in open(test_file):
                 countAll += counter[featureID+1][fields[featureID]]
             except:
                 pass
-    # te_vec.append(data+[countAll])
     fOut.write( data + ',' + str(countAll) + '\n' )
 fOut.close()
